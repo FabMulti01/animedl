71e14e178a0fabfe4f644bac8e19e78de8ed1360
Fabio <90962331+FabMulti01@users.noreply.github.com>
2022-07-23 19:46:24 +0200

    Delete src directory

--- src/renderer.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { AnimeDL } from "./components/AnimeDL";
-import "./styles/main.css";
-
-ReactDOM.render(<AnimeDL />, document.getElementById("app"));
